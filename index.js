b86a70e7e00d9b49822359d261d188809e082d1f
hamzailyas237 <hamzailyas237@gmail.com>
2023-01-11 21:48:11 +0500

    products api

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const app = require('express')()
+const PORT = 5000
+require('dotenv').config()
+const mongoose = require('mongoose')
+const { getAllProducts, getProductById, getProductsByType } = require('./controllers/productController')
+
+
+mongoose.connect(process.env.URI)
+    .then(() => {
+        console.log('MongoDB Connected')
+    })
+    .catch((error) => {
+        console.log('MongoDB Connection Error', error.message)
+    })
+
+
+app.get('/products', getAllProducts)
+app.get('/products/:id', getProductById)
+app.get('/products/type/:type', getProductsByType)
+
+
+app.listen(PORT)
+console.log(`Server running at https://localhost:${PORT}`)
